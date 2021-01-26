44f7469633eb0edac25f8bcdacc85c71d60e48ea
Sascha Helmund <helmund@mogic.com>
2021-01-26 21:18:03 +0100

    added html lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    htmlAttrs: {
+      lang: 'de'
+    },
     title: 'Sascha Helmund',
     meta: [
       { charset: 'utf-8' },
